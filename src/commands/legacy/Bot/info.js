2b090caaa01d3465d387cc5571d20541b76bf464
Noah <elinoah16@gmail.com>
2022-08-23 19:41:58 +0100

    updated info legacy 23-08-2022

--- src/commands/legacy/Bot/info.js
+++ src/commands/legacy/Bot/info.js
@@ -14,8 +14,8 @@ module.exports = {
         let seconds = Math.floor(bot.uptime / 1000) % 60;
 
         const date = new Date();
-        let tZ = 'en-US'
-        let londonDate = date.toLocaleString(`\`${tZ}\``, { timeZone: 'Europe/London', timeZoneName: 'short', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' })
+        
+        let londonDate = date.toLocaleString('en-US')
 
         let infoEmbed = new bot.discord.MessageEmbed()
         .setAuthor({ name: `◠ Information ◡`, iconURL: bot.user.displayAvatarURL() })
