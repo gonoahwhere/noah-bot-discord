75800aa585ba4759f0fd58d2065c841247de879e
Noah <elinoah16@gmail.com>
2022-08-24 00:59:13 +0100

    added better sqlite3 24-08-2022

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -3,13 +3,13 @@ const { QuickDB } = require('quick.db');
 const db = new QuickDB();
 
 // db.fetch()
-const getHugCooldown = (guildId, userId, usersId) => db.fetch(`hug_${guildId}_${userId}_${usersId}`)
+const getHugCooldown = (guildId, userId, usersId) => await db.fetch(`hug_${guildId}_${userId}_${usersId}`)
 // db.add()
 
 // db.subtract()
 
 // db.set()
-const setHugCooldown = (guildId, userId, usersId) => db.set(`hug_${guildId}_${userId}_${usersId}`)
+const setHugCooldown = (guildId, userId, usersId) => await db.set(`hug_${guildId}_${userId}_${usersId}`)
 
 module.exports = {
     getHugCooldown,
