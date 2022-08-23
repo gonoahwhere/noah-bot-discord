fa1baa29366896d7611df299033ed019551af66f
Noah <elinoah16@gmail.com>
2022-08-23 01:36:31 +0100

    added help.js for slash commands 23-08-2022

---
+++ src/commands/slash/Bot/help.js
@@ -0,0 +1,78 @@
+const { readdirSync } = require("fs");
+
+// Example of how to make a Help SlashCommand
+
+module.exports = {
+    name: "help",
+    usage: '/help <command>',
+    options: [
+        {
+            name: 'command',
+            description: 'What command do you need help',
+            type: 'STRING',
+            required: false
+        }
+    ],
+    category: "Bot",
+    description: "Return all commands, or one specific command!",
+    ownerOnly: false,
+    run: async (bot, interaction) => {
+
+        // Buttons that take you to a link
+        // If you want to delete them, remove this part of
+        // the code and in line: 62 delete ", components: [row]"
+        const row = new bot.discord.MessageActionRow()
+            .addComponents(
+                new bot.discord.MessageButton()
+                    .setLabel("GitHub")
+                    .setStyle("LINK")
+                    .setURL("http://github.com/gonoahwhere/noah-bot-discord")
+            );
+
+        const commandInt = interaction.options.getString("command");
+        if (!commandInt) {
+
+            // Get the commands of a Bot category
+            const botCommandsList = [];
+            readdirSync(`${bot.cwd}/src/commands/slash/Bot`).forEach((file) => {
+                const filen = require(`${bot.cwd}/src/commands/slash/Bot/${file}`);
+                const name = `\`${filen.name}\``;
+                botCommandsList.push(name);
+            });
+
+            // This is what it commands when using the command without arguments
+            const helpEmbed = new bot.discord.MessageEmbed()
+                .setTitle(`Hissssss... Let me help you!`)
+                .setDescription(` Hello **<@${interaction.member.id}>**, I am the droid version of sir noah...  \nYou can use \`/help command>\` to see more info about about a certain slash command!\n**Total Commands:** ${bot.commands.size}\n**Total Slash Commands:** ${bot.slash.size}`)
+                .addField("Default", botCommandsList.map((data) => `${data}`).join(", "), true)
+                .setColor(bot.config.embedColor)
+                .setFooter({ text: `${bot.config.embedfooterText}`, iconURL: `${bot.user.displayAvatarURL()}` });
+
+            interaction.reply({ embeds: [helpEmbed], components: [row] });
+        } else {
+            const command = bot.slash.get(commandInt.toLowerCase());
+
+            if (!command) {
+                interaction.reply({ content: `There isnt a slash command named "${commandInt}"` });
+            } else {
+
+                let command = bot.slash.get(commandInt.toLowerCase());
+                let name = command.name;
+                let description = command.description || "No descrpition provided"
+                let usage = command.usage || "No usage provided"
+                let category = command.category || "No category provided!"
+
+                let helpCmdEmbed = new bot.discord.MessageEmbed()
+                    .setTitle(`${bot.user.username} Help | \`${(name.toLocaleString())}\` Slash Command`)
+                    .addFields(
+                        { name: "Description", value: `${description}` },
+                        { name: "Usage", value: `${usage}` },
+                        { name: 'Category', value: `${category}` })
+                    .setColor(bot.config.embedColor)
+                    .setFooter({ text: `${bot.config.embedfooterText}`, iconURL: `${bot.user.displayAvatarURL()}` });
+
+                interaction.reply({ embeds: [helpCmdEmbed] });
+            }
+        }
+    },
+};
