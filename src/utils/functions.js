75bcfce610026ad270d3270e929bf8243814e7ec
Noah <elinoah16@gmail.com>
2022-08-24 01:06:11 +0100

    fixed the db fetch 24-08-2022

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -3,13 +3,13 @@ const { QuickDB } = require('quick.db');
 const db = new QuickDB();
 
 // db.fetch()
-const getHugCooldown = (guildId, userId, usersId) => db.fetch(`hug_${guildId}_${userId}_${usersId}`)
+const getHugCooldown = (guildId, userId) => db.fetch(`userhugcooldown_${guildId}_${userId}`)
 // db.add()
 
 // db.subtract()
 
 // db.set()
-const setHugCooldown = (guildId, userId, usersId) => db.set(`hug_${guildId}_${userId}_${usersId}`)
+const setHugCooldown = (guildId, userId) => db.set(`userhugcooldown_${guildId}_${userId}`)
 
 module.exports = {
     getHugCooldown,
