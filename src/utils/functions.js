dd83b184cb8ab2924419aea761e74e02205454e0
Noah <elinoah16@gmail.com>
2022-08-24 03:20:23 +0100

    fixed cooldowns not working 24-08-2022

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -5,6 +5,10 @@ const db = new QuickDB();
 const getHugCooldown = (guildId, userId) => db.get(`userhugcooldown_${guildId}_${userId}`)
 const getInfoCooldown = (guildId, userId) => db.get(`userinfocooldown_${guildId}_${userId}`)
 const getHelpCooldown = (guildId, userId) => db.get(`userhelpcooldown_${guildId}_${userId}`)
+
+const getHugSlashCooldown = (guildId, userId) => db.get(`userhugslashcooldown_${guildId}_${userId}`)
+const getInfoSlashCooldown = (guildId, userId) => db.get(`userinfoslashcooldown_${guildId}_${userId}`)
+const getHelpSlashCooldown = (guildId, userId) => db.get(`userhelpslashcooldown_${guildId}_${userId}`)
 // db.add()
 
 // db.subtract()
@@ -14,12 +18,24 @@ const setHugCooldown = (guildId, userId, date) => db.set(`userhugcooldown_${guil
 const setInfoCooldown = (guildId, userId, date) => db.set(`userinfocooldown_${guildId}_${userId}`, date)
 const setHelpCooldown = (guildId, userId, date) => db.set(`userhelpcooldown_${guildId}_${userId}`, date)
 
+const setHugSlashCooldown = (guildId, userId, date) => db.set(`userhugslashcooldown_${guildId}_${userId}`, date)
+const setInfoSlashCooldown = (guildId, userId, date) => db.set(`userinfoslashcooldown_${guildId}_${userId}`, date)
+const setHelpSlashCooldown = (guildId, userId, date) => db.set(`userhelpslashcooldown_${guildId}_${userId}`, date)
+
 module.exports = {
     getHugCooldown,
     getInfoCooldown,
     getHelpCooldown,
 
+    getHugSlashCooldown,
+    getInfoSlashCooldown,
+    getHelpSlashCooldown,
+
     setHugCooldown,
     setInfoCooldown,
     setHelpCooldown,
+
+    setHugSlashCooldown,
+    setInfoSlashCooldown,
+    setHelpSlashCooldown,
 }
