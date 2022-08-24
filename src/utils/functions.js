08df5f31904f01b59e56da9897bcc191f27ae016
Noah <elinoah16@gmail.com>
2022-08-24 02:13:15 +0100

    trying to fix db.fetch is not a function 24-08-2022

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,13 +2,13 @@ const { QuickDB } = require('quick.db');
 const db = new QuickDB();
 
 // db.fetch()
-const getHugCooldown = (guildId, userId) = await db.fetch(`userhugcooldown_${guildId}_${userId}`)
+const getHugCooldown = (guildId, userId) => db.get(`userhugcooldown_${guildId}_${userId}`)
 // db.add()
 
 // db.subtract()
 
 // db.set()
-const setHugCooldown = (guildId, userId) = await db.set(`userhugcooldown_${guildId}_${userId}`)
+const setHugCooldown = (guildId, userId) => db.set(`userhugcooldown_${guildId}_${userId}`)
 
 module.exports = {
     getHugCooldown,
